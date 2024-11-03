cf41f759d803424431f78b17e7a6ebfa01d2adf4
Graca-OAWEB <gracasantosmariana@hmail.com>
2024-11-02 22:43:30 -0300

    list

--- js/main.js
+++ js/main.js
@@ -1,17 +1,39 @@
-const scrollContainer = document.querySelector('.scroll-container');
+//------------------------------------------------------
 
-document.getElementById('scrollLeft').addEventListener('click', () => {
-    scrollContainer.scrollBy({
-        top: 0,
-        left: -200, // Ajuste a quantidade para a velocidade de rolagem
-        behavior: 'smooth'
-    });
-});
+//CARROSSEL
 
-document.getElementById('scrollRight').addEventListener('click', () => {
-    scrollContainer.scrollBy({
-        top: 0,
-        left: 200, // Ajuste a quantidade para a velocidade de rolagem
-        behavior: 'smooth'
+function initializeCarrossel(carrosselSelector, btnProximoSelector, btnAnteriorSelector) {
+    const carrossel = document.querySelector(carrosselSelector);
+    const btnProximo = document.querySelector(btnProximoSelector);
+    const btnAnterior = document.querySelector(btnAnteriorSelector);
+
+    let index = 0;
+    const listItemMenu = carrossel.querySelectorAll('.listItemMenu');
+    const numProducts = listItemMenu.length;
+    let productWidth = listItemMenu[0].offsetWidth;
+
+    function moveCarrossel() {
+        carrossel.style.transform = `translateX(${-index * productWidth}px)`;
+    }
+
+    btnProximo.addEventListener('click', () => {
+        index = (index + 1) % numProducts;
+        moveCarrossel();
     });
-});
+
+    btnAnterior.addEventListener('click', () => {
+        index = (index - 1 + numProducts) % numProducts;
+        moveCarrossel();
+    });
+
+    window.addEventListener('resize', () => {
+        productWidth = listItemMenu[0].offsetWidth;
+        moveCarrossel();
+    });
+}
+
+initializeCarrossel(
+    '.scroll-container',
+    '.btn-proximo2',
+    '.btn-anterior2'
+);
